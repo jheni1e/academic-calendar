134d19d08151d6a04686f6743d6b0e5246580166
Jhenie <highhalma@gmail.com>
2026-06-22 08:27:11 -0300

    FEAT: calendar components added

---
+++ academic-calendar/src/components/MonthlyCalendar/index.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import DayCell from "../DayCell";
+import "./index.css";
+import DropdownList from "../../components/DropdownList";
+import Toggle from "../Toggle";
+import BoschButton from "../BoschButton";
+
+const months = [
+  "Janeiro",
+  "Fevereiro",
+  "Março",
+  "Abril",
+  "Maio",
+  "Junho",
+  "Julho",
+  "Agosto",
+  "Setembro",
+  "Outubro",
+  "Novembro",
+  "Dezembro",
+];
+
+export default function MonthlyCalendar() {
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  const year = currentDate.getFullYear();
+  const month = currentDate.getMonth();
+
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+  const firstDayOfMonth = new Date(year, month, 1).getDay();
+
+  const emptyCells = Array.from({ length: firstDayOfMonth });
+
+  const days = Array.from({ length: daysInMonth }, (_, i) => i + 1);
+
+  const monthOptions = months.map((monthName, index) => ({
+    label: monthName,
+    value: index,
+  }));
+
+  return (
+    <div className="calendar-container">
+      <div className="calendar-header">
+        <div className="actions">
+          <BoschButton text="<" type="secondary" />
+          <BoschButton text=">" type="secondary" />
+        </div>
+        <h1 style={{ fontSize: '2rem', fontWeight: 'normal' }}>
+          {months[month]} {year}
+        </h1>
+
+        <div className="actions">
+          <BoschButton text="+" type="secondary" />
+          <Toggle leftText="Mensal" rightText="Semanal" />
+        </div>
+      </div>
+
+      <div className="weekdays">
+        <span>DOM</span>
+        <span>SEG</span>
+        <span>TER</span>
+        <span>QUA</span>
+        <span>QUI</span>
+        <span>SEX</span>
+        <span>SAB</span>
+      </div>
+
+      <div className="calendar-grid">
+        {emptyCells.map((_, index) => (
+          <div key={`empty-${index}`} className="empty-cell" />
+        ))}
+
+        {days.map((day) => (
+          <DayCell key={day} day={day} events={[]} />
+        ))}
+      </div>
+    </div>
+  );
+}
