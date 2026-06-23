9273aeb3f61f266fd9b965ca0809111910d4406e
BecaIanz <silveiraianzr@gmail.com>
2026-06-23 11:45:05 -0300

    FEAT: create Classes Page

--- academic-calendar/src/pages/ClassesPage/index.jsx
+++ academic-calendar/src/pages/ClassesPage/index.jsx
@@ -3,9 +3,17 @@ import "./index.css";
 import { useState } from "react";
 
 function Classes() {
+  
+  const classes = [
+    {name: "DTA", reponsible: "Queila Lima", initial: "03/02/2025", end: "03/08/2025"},
+    {name: "Analise", reponsible: "Queila Lima", initial: "03/08/2025", end: "10/12/2026"},
+    {name: "Manufatura", reponsible: "Gabriel Bernadelli",  initial: "03/08/2025", end: "10/12/2026"}
+  ]
   return (
     <>
-    <CardClass NameClass={"DTA"} InitialDate={"03/02/2025"} EndDate={"03/08/2026"} Responsible={"Queila Lima"}></CardClass>
+      {classes.map((classe)=> (
+        <CardClass NameClass={classe.name} Responsible={classe.reponsible} InitialDate={classe.initial} EndDate={classe.end}></CardClass>
+      ))}
     </>
   );
 }
