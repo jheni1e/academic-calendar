c2a056a13e4ba65715519f2c66d567ce206d2ad0
BecaIanz <silveiraianzr@gmail.com>
2026-06-23 09:32:14 -0300

    FEAT: competed View Subject Component

--- academic-calendar/src/components/ViewSubjectComponent/index.jsx
+++ academic-calendar/src/components/ViewSubjectComponent/index.jsx
@@ -1,12 +1,12 @@
 import "./index.css";
 import CircleChartItem from "../CircleChartItem"
 
-function ViewSubjectComponent({SubjectName, Responsible, Percentage}) {
+function ViewSubjectComponent({SubjectName, Responsible, Class, Percentage}) {
   return (
     <>
       <div className="component-container">
-        <div>
-            <h1>{SubjectName}</h1>
+        <div className="subject-name">
+            <h1>{SubjectName} - {Class}</h1>
             <h4>{Responsible}</h4>
         </div>
         <CircleChartItem percentage={Percentage} color1={"gray"} color2={"black"}></CircleChartItem>
