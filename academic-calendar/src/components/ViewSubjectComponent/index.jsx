d3e8bbdbdc2c918c62066c54b99b9ca5f784c112
BecaIanz <silveiraianzr@gmail.com>
2026-06-23 10:15:24 -0300

    FIX: fix View Subject Component Error

--- academic-calendar/src/components/ViewSubjectComponent/index.jsx
+++ academic-calendar/src/components/ViewSubjectComponent/index.jsx
@@ -1,16 +1,11 @@
 import "./index.css";
 import CircleChartItem from "../CircleChartItem"
 import { useState } from "react";
-import ViewSubjectModal from "../ViewSubjectModal";
 
 function ViewSubjectComponent({SubjectName, Responsible, Class, Percentage}) {
-  const [isModalOpen, setIsModalOpen] = useState(false)
   return (
     <>
-      <div className="component-container" onClick={() => setIsModalOpen(true)}>
-        {isModalOpen &&
-          <ViewSubjectModal></ViewSubjectModal>
-        }
+      <div className="component-container">
         <div className="subject-name">
             <h1>{SubjectName} - {Class}</h1>
             <h4>{Responsible}</h4>
