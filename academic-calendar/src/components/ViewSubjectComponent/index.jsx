439d1a3d17905feb25a2db46cc9c41ce6f25977c
BecaIanz <silveiraianzr@gmail.com>
2026-06-23 10:13:04 -0300

    CHORE: remove unused folders

--- academic-calendar/src/components/ViewSubjectComponent/index.jsx
+++ academic-calendar/src/components/ViewSubjectComponent/index.jsx
@@ -1,10 +1,16 @@
 import "./index.css";
 import CircleChartItem from "../CircleChartItem"
+import { useState } from "react";
+import ViewSubjectModal from "../ViewSubjectModal";
 
 function ViewSubjectComponent({SubjectName, Responsible, Class, Percentage}) {
+  const [isModalOpen, setIsModalOpen] = useState(false)
   return (
     <>
-      <div className="component-container">
+      <div className="component-container" onClick={() => setIsModalOpen(true)}>
+        {isModalOpen &&
+          <ViewSubjectModal></ViewSubjectModal>
+        }
         <div className="subject-name">
             <h1>{SubjectName} - {Class}</h1>
             <h4>{Responsible}</h4>
