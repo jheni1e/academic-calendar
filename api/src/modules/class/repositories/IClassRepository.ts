5152c1daa21e7c026df3e8eecafea7774d2b6213
Fialho Fernanda (CtP/ETS) <fif1ct@bosch.com>
2026-06-23 13:27:54 -0300

    feat: class dtos and repository

---
+++ api/src/modules/class/repositories/IClassRepository.ts
@@ -0,0 +1,17 @@
+import { CreateClassDTO, UpdateClassDTO } from "../ClassDto.ts";
+
+export interface IClassRepository {
+
+    create(data: CreateClassDTO) : Promise<Class>;
+
+    findById(classId: number) : Promise<Class>;
+
+    findAll() : Promise<Class>;
+
+    update(
+        classId: number,
+        data: UpdateClassDTO
+    ) : Promise<Class>;
+
+    delete(classId: number) : Promise<Class>;
+}
