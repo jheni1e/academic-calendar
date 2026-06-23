2653ea392146703a30ece59db54e0feedb560701
Fialho Fernanda (CtP/ETS) <fif1ct@bosch.com>
2026-06-23 13:47:24 -0300

    feat: assignment dtos and repository

---
+++ api/src/modules/assignment/repositories/IAssignmentRepository.ts
@@ -0,0 +1,22 @@
+import { CreateAssignmentDTO } from "../AssignmentDto.ts";
+
+export interface IAssignmentRepository {
+
+    create(
+        data: CreateAssignmentDTO
+    ): Promise<Assignment>;
+
+    findById(
+        assignmentId: number
+    ): Promise<Assignment | null>;
+
+    findAll(): Promise<Assignment[]>;
+
+    findByUserId(
+        userId: number
+    ): Promise<Assignment[]>;
+    
+    delete(
+        assignmentId: number
+    ): Promise<void>;
+}
---
+++ api/src/modules/assignment/repositories/PrismaAssignmentRepository.ts
@@ -0,0 +1,56 @@
+import { prisma } from "../../../lib/prisma.ts";
+import { CreateAssignmentDTO } from "../AssignmentDto.ts";
+import { IAssignmentRepository } from "./IAssignmentRepository.ts";
+
+export class PrismaAssignmentRepository implements IAssignmentRepository {
+
+    async create(
+        data: CreateAssignmentDTO
+    ): Promise<Assignment> {
+
+        return prisma.assignment.create({
+            data: {
+                role_id: data.roleId,
+                user_id: data.userId
+            }
+        });
+    }
+
+    async findById(
+        assignmentId: number
+    ): Promise<Assignment | null> {
+
+        return prisma.assignment.findUnique({
+            where: {
+                assignment_id: assignmentId
+            }
+        });
+    }
+
+    async findAll(): Promise<Assignment[]> {
+
+        return prisma.assignment.findMany();
+    }
+
+    async findByUserId(
+        userId: number
+    ): Promise<Assignment[]> {
+
+        return prisma.assignment.findMany({
+            where: {
+                user_id: userId
+            }
+        });
+    }
+
+    async delete(
+        assignmentId: number
+    ): Promise<void> {
+
+        await prisma.assignment.delete({
+            where: {
+                assignment_id: assignmentId
+            }
+        });
+    }
+}
