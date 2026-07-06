d5e5e45f09a9a4926be6e4af6582106f96c084ab
Jhenie <highhalma@gmail.com>
2026-07-06 08:59:34 -0300

    FIX: routes separated, with and without header

--- academic-calendar/src/routes/index.jsx
+++ academic-calendar/src/routes/index.jsx
@@ -1,20 +1,42 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Home from "../pages/HomePage";
 import NotFound from "../pages/NotFoundPage";
 import Subject from "../pages/SubjectPage";
+import Register from "../pages/RegisterPage";
+
+function WithHeader() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
+}
+
+function WithoutHeader() {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+}
 
 export default function AppRouter() {
   return (
     <Router basename="/calendario-academico">
-      <Header />
       <Routes>
-        <Route path="/materias" element={<Subject/>} />
-        <Route path="/home" element={<Home/>} />
-        {/* <Route path="/group" element={ <Courses/>} />
-        <Route path="/group/:eventId" element={ <Classes/>} /> */}
-        <Route path="/*" element={<NotFound />} />
+        <Route element={<WithHeader />}>
+          <Route path="/materias" element={<Subject />} />
+          <Route path="/home" element={<Home />} />
+          {/* <Route path="/group" element={ <Courses/>} />
+          <Route path="/group/:eventId" element={ <Classes/>} /> */}
+          <Route path="/*" element={<NotFound />} />
+        </Route>
+        <Route element={<WithoutHeader />}>
+          <Route path="/register" element={<Register />} />
+        </Route>
       </Routes>
     </Router>
   );
