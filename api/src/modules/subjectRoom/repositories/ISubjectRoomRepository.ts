44e57776c9c092c071243ee2ece97f3fb56f7211
FehFialho <fernandaklechowicz@gmail.com>
2026-07-09 09:15:43 -0300

    feat: create subject_room repos, dto, structure

---
+++ api/src/modules/subjectRoom/repositories/ISubjectRoomRepository.ts
@@ -0,0 +1,40 @@
+import { SubjectRoom } from "../../../generated/prisma/client.ts";
+import {
+    CreateSubjectRoomDTO,
+    UpdateSubjectRoomDTO
+} from "../SubjectRoomDTO.ts";
+
+export interface ISubjectRoomRepository {
+
+    create(
+        data: CreateSubjectRoomDTO
+    ): Promise<SubjectRoom>;
+
+    findById(
+        subjectRoomId: number
+    ): Promise<SubjectRoom | null>;
+
+    findBySubject(
+        subjectId: number
+    ): Promise<SubjectRoom[]>;
+
+    findByRoom(
+        roomId: number
+    ): Promise<SubjectRoom[]>;
+
+    findBySubjectAndRoom(
+        subjectId: number,
+        roomId: number
+    ): Promise<SubjectRoom | null>;
+
+    findAll(): Promise<SubjectRoom[]>;
+
+    update(
+        subjectRoomId: number,
+        data: UpdateSubjectRoomDTO
+    ): Promise<SubjectRoom>;
+
+    delete(
+        subjectRoomId: number
+    ): Promise<void>;
+}
