16d83e96585b9807019530664e42bbf62d60b540
FehFialho <fernandaklechowicz@gmail.com>
2026-07-09 10:28:01 -0300

    feat: event_role basic usecases

---
+++ api/src/modules/eventRole/usecases/UpdateEventRoleUseCase.ts
@@ -0,0 +1,30 @@
+import { IEventRoleRepository } from "../repositories/IEventRoleRepository.ts";
+import { UpdateEventRoleDTO } from "../EventRoleDTO.ts";
+
+export class UpdateEventRoleUseCase {
+    constructor(
+        private readonly eventRoleRepository: IEventRoleRepository
+    ) {}
+
+    async execute(
+        eventRoleId: number,
+        data: UpdateEventRoleDTO
+    ) {
+
+        const eventRole =
+            await this.eventRoleRepository.findById(eventRoleId);
+
+        if (!eventRole) {
+            throw new Error("Papel do evento não encontrado.");
+        }
+
+        if (data.name !== undefined && !data.name.trim()) {
+            throw new Error("Nome do papel do evento é obrigatório.");
+        }
+
+        return await this.eventRoleRepository.update(
+            eventRoleId,
+            data
+        );
+    }
+}
