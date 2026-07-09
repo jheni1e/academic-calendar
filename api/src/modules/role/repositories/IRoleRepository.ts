7af7e6abc62f0296c13fad97ede975ac333726f1
FehFialho <fernandaklechowicz@gmail.com>
2026-07-09 09:15:22 -0300

    feat: create role  repos, dto, structure

---
+++ api/src/modules/role/repositories/IRoleRepository.ts
@@ -0,0 +1,27 @@
+import { Role } from "../../../generated/prisma/client.ts";
+import {
+    CreateRoleDTO,
+    UpdateRoleDTO
+} from "../RoleDTO.ts";
+
+export interface IRoleRepository {
+
+    create(
+        data: CreateRoleDTO
+    ): Promise<Role>;
+
+    findById(
+        roleId: number
+    ): Promise<Role | null>;
+
+    findAll(): Promise<Role[]>;
+
+    update(
+        roleId: number,
+        data: UpdateRoleDTO
+    ): Promise<Role>;
+
+    delete(
+        roleId: number
+    ): Promise<void>;
+}
