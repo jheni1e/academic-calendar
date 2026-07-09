9ff905bb5e575479a1628070f13b2b3d2d17924f
Jhenie <highhalma@gmail.com>
2026-07-09 09:24:01 -0300

    FIX: routes updated

--- academic-calendar/src/routes/index.jsx
+++ academic-calendar/src/routes/index.jsx
@@ -7,12 +7,19 @@ import Subject from "../pages/SubjectPage";
 import Classes from "../pages/ClassesPage";
 import Class from "../pages/ClassPage";
 import Register from "../pages/RegisterPage";
+import Semester from "../pages/SemesterPage";
+import './index.css'
 
 function WithHeader() {
   return (
     <>
-      <Header />
-      <Outlet />
+      <div className="layout">
+        <Header />
+
+        <main className="layout-content">
+          <Outlet />
+        </main>
+      </div>
     </>
   );
 }
@@ -29,14 +36,12 @@ export default function AppRouter() {
   return (
     <Router basename="/calendario-academico">
       <Routes>
-          <Route path="/*" element={<NotFound />} />
-          <Route element={<WithHeader />}>
+        <Route path="/*" element={<NotFound />} />
+        <Route element={<WithHeader />}>
           <Route path="/materias" element={<Subject />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/turmas" element={<Classes/>} />
-          {/* <Route path="/turmas/:name" element={<Class/>} /> */}
-          {/* <Route path="/group" element={ <Courses/>} />
-          <Route path="/group/:eventId" element={ <Classes/>} /> */}
+          <Route path="/turmas" element={<Classes />} />
+          <Route path="/semestre" element={<Semester />} />
           <Route path="/*" element={<NotFound />} />
         </Route>
         <Route element={<WithoutHeader />}>
