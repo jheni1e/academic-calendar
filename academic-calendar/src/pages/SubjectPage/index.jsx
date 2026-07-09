e9f00c55cf156b6864f9cbc6aac6e1ff46de4d90
Jhenie <highhalma@gmail.com>
2026-07-09 09:25:32 -0300

    FIX: a lot of css was fixed so the menu sidebar can work on both the home and the subjects page

--- academic-calendar/src/pages/SubjectPage/index.jsx
+++ academic-calendar/src/pages/SubjectPage/index.jsx
@@ -7,12 +7,12 @@ import { useState } from "react";
 function Subject() {
   const [selectedValue, setSelectedValue] = useState("");
   const subjects = [
-    {name: "IOT", reponsible: "Patrick", class: "dta", percentage: 78},
-    {name: "Projeto FullStack", reponsible: "Cristian", class: "dta", percentage: 98},
-    {name: "Excell", reponsible: "Queila", class: "analise", percentage: 68},
-    {name: "Python", reponsible: "Queila", class: "analise", percentage: 47},
+    { name: "IOT", reponsible: "Patrick", class: "dta", percentage: 78 },
+    { name: "Projeto FullStack", reponsible: "Cristian", class: "dta", percentage: 98 },
+    { name: "Excell", reponsible: "Queila", class: "analise", percentage: 68 },
+    { name: "Python", reponsible: "Queila", class: "analise", percentage: 47 },
   ]
-  
+
   const listMenu = [
     { value: "analise", label: "Análise" },
     { value: "dta", label: "DTA" },
@@ -23,27 +23,41 @@ function Subject() {
   return (
     <>
       <div className="container-page">
-        <MenuSideBar 
-          hasToggle={false} 
-          hasDropDown={true} 
-          OptionsDropDown={listMenu} 
-          LabelDropDown={"Turmas"} 
+        <MenuSideBar
+          hasToggle={false}
+          hasDropDown={true}
+          OptionsDropDown={listMenu}
+          LabelDropDown="Turmas"
           selectedValueDrop={selectedValue}
-          onDropDownChange={(e) => setSelectedValue(e.target.value)}></MenuSideBar>
+          onDropDownChange={(e) => setSelectedValue(e.target.value)}
+        />
+
         <div className="container-subjects">
           <div className="title">
-            <h1 >Todas as Matérias</h1>
-            <div className="button">
-              <BoschButton text={"+  Adicionar Matéria"} type={"secondary"}></BoschButton>
-            </div>
+            <h1>Todas as Matérias</h1>
+
+            <BoschButton
+              text="+ Adicionar Matéria"
+              type="secondary"
+              style={{width: "250px"}}
+            />
           </div>
-          {subjects
-            .filter(subject =>
-              selectedValue === "" || subject.class === selectedValue
-            )
-            .map((subject)=> (
-              <ViewSubjectComponent SubjectName={subject.name} Responsible={subject.reponsible} Percentage={subject.percentage} Class={subject.class}></ViewSubjectComponent>
-          ))}
+
+          <div className="subjects-list">
+            {subjects
+              .filter(subject =>
+                selectedValue === "" || subject.class === selectedValue
+              )
+              .map(subject => (
+                <ViewSubjectComponent
+                  key={subject.name}
+                  SubjectName={subject.name}
+                  Responsible={subject.reponsible}
+                  Percentage={subject.percentage}
+                  Class={subject.class}
+                />
+              ))}
+          </div>
         </div>
       </div>
     </>
