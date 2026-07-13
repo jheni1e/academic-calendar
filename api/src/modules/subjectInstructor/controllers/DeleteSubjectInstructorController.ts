23e93e92585d84aec51821100acb0d3edc1f9eb2
Leehzitha <leticia.burlinski@gmail.com>
2026-07-13 11:01:54 -0300

    fix: Subject room controller fixed

--- api/src/modules/subjectInstructor/controllers/DeleteSubjectInstructorController.ts
+++ api/src/modules/subjectInstructor/controllers/DeleteSubjectInstructorController.ts
@@ -9,32 +9,7 @@ export class DeleteSubjectInstructorController {
 
     private readonly repository = new PrismaSubjectInstructorRepository();
 
-    private readonly useCase = new DeleteSubjectInstructorUseCase(this.repository);
-
-    async handle(req: Request, res: Response) {
-
-        try {
-
-            await this.useCase.execute(
-                Number(req.params.id)
-            );
-
-            return res.sendStatus(204);
-
-        } catch (error) {
-
-            if (error instanceof AppError) {
-                return res.status(error.statusCode).json({
-                    message: error.message
-                });
-            }
-
-            return res.status(500).json({
-                message: "Internal server error."
-            });
-
-        }
-
-    }
+    
+   
 
 }
