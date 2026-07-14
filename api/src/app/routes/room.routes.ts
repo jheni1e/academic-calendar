7bee13dd0bae0dae190730bcfe17ac576b5ff778
Leehzitha <leticia.burlinski@gmail.com>
2026-07-14 11:43:09 -0300

    feat: create findByRoom controller on reservation

--- api/src/app/routes/room.routes.ts
+++ api/src/app/routes/room.routes.ts
@@ -3,16 +3,24 @@ import { RoomController } from '../../modules/room/controllers/RoomController.ts
 import { authMiddleware } from '../../shared/middlewares/auth.middleware.ts';
 import { authorize } from '../../shared/middlewares/authorization.middleware.ts';
 import { Role } from '../../shared/enums/role.ts';
+import { ReservationController } from '../../modules/reservation/controllers/ReservationController.ts';
 
 const route = express.Router();
 
 const roomController = new RoomController();
+const reservationController = new ReservationController();
 
 route 
     .post('/', authMiddleware, authorize(Role.ADMIN, Role.INSTRUCTOR), roomController.create)
+    .post('/reservation/:roomId', authMiddleware, authorize(Role.ADMIN, Role.INSTRUCTOR), reservationController.create)
+    
     .get('/all', authMiddleware, roomController.getAll)
     .get('/:id', authMiddleware, roomController.getById)
+    .get('/reservations', authMiddleware, reservationController.getById)
+    
     .put('/:id', authMiddleware, authorize(Role.ADMIN, Role.INSTRUCTOR), roomController.update)
+
+    .delete('/reservation/:roomId', authMiddleware, authorize(Role.ADMIN, Role.INSTRUCTOR), reservationController.delete)
     .delete("/:id", authMiddleware, authorize(Role.ADMIN, Role.INSTRUCTOR), roomController.delete)
 
 export default route
--- api/src/modules/reservation/repositories/IReservationRepository.ts
+++ api/src/modules/reservation/repositories/IReservationRepository.ts
@@ -7,6 +7,8 @@ export interface IReservationRepository {
 
     findAll() : Promise<Reservation[]>;
 
+    findByRoom(roomId : number) : Promise<Reservation[] | null>;
+
     update(
         reservationId: number,
         data: UpdateReservationDTO
