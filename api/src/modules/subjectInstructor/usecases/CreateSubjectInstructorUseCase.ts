9f40c6b540733782efdcfd4eac0a690afe8771e7
FehFialho <fernandaklechowicz@gmail.com>
2026-07-14 08:27:00 -0300

    feat: update create subject_instructor usecase

--- api/src/modules/subjectInstructor/usecases/CreateSubjectInstructorUseCase.ts
+++ api/src/modules/subjectInstructor/usecases/CreateSubjectInstructorUseCase.ts
@@ -1,16 +1,32 @@
+import { ISubjectRepository } from "../../subject/repositories/ISubjectRepository.ts";
+import { IUserRepository } from "../../user/repositories/IUserRepository.ts";
 import { ISubjectInstructorRepository } from "../repositories/ISubjectInstructorRepository.ts";
 import { CreateSubjectInstructorDTO } from "../SubjectInstructorDto.ts";
 
 export class CreateSubjectInstructorUseCase {
     constructor(
-        private readonly subjectInstructorRepository: ISubjectInstructorRepository
+        private readonly subjectInstructorRepository: ISubjectInstructorRepository,
+        private readonly subjectRepository: ISubjectRepository,
+        private readonly userRepository: IUserRepository
     ) {}
 
     async execute(data: CreateSubjectInstructorDTO) {
 
-        // TODO: Verificar se a matéria existe.
+        // Verificar se a matéria existe.
+        const subject = await this.subjectRepository.findById(
+            data.subjectId
+        )
+        if (!subject) {
+            throw new Error("Subject doesn't exist!");
+        }
 
-        // TODO: Verificar se o usuário existe.
+        // Verificar se o usuário existe.
+        const user = await this.userRepository.findById(
+            data.instructorId
+        )
+        if (!user) {
+            throw new Error("Instructor doesn't exist!");
+        }
 
         const relation = await this.subjectInstructorRepository.findBySubjectAndInstructor(
             data.subjectId,
