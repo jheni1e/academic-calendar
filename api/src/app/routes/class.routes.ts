c94db3766fe1a8d0dddf3f2f9bc0fd6dc98523df
Leehzitha <leticia.burlinski@gmail.com>
2026-07-14 09:24:43 -0300

    feat: settled class authentication middleware

--- api/src/app/routes/class.routes.ts
+++ api/src/app/routes/class.routes.ts
@@ -1,6 +1,9 @@
 import express from 'express'
 import { ClassController } from '../../modules/class/controllers/ClassController.ts';
 import { ClassUserController } from '../../modules/classUser/controllers/ClassUserControllers.ts';
+import { authMiddleware } from '../../shared/middlewares/auth.middleware.ts';
+import { Role } from '../../shared/enums/role.ts';
+import { authorize } from '../../shared/middlewares/authorization.middleware.ts';
 
 const route = express.Router();
 
@@ -9,11 +12,11 @@ const classUserController = new ClassUserController();
 
 route 
     .post('/', classController.create)
-    .get('/all', classController.getAll)
-    .get('/:id', classController.getById)
-    .put('/:id', classController.update)
-    .post('/participants', classUserController.create)
-    .delete('/participants/:id', classUserController.delete)
-    .delete("/:id", classController.delete)
+    .get('/all', authMiddleware, authorize(Role.ADMIN, Role.INSTRUCTOR), classController.getAll)
+    .get('/:id', authMiddleware, authorize(Role.ADMIN, Role.INSTRUCTOR), classController.getById)
+    .put('/:id', authMiddleware, authorize(Role.ADMIN, Role.INSTRUCTOR), classController.update)
+    .post('/participants', authMiddleware, authorize(Role.ADMIN, Role.INSTRUCTOR), classUserController.create)
+    .delete('/participants/:id', authMiddleware, authorize(Role.ADMIN, Role.INSTRUCTOR), classUserController.delete)
+    .delete("/:id", authMiddleware, authorize(Role.ADMIN, Role.INSTRUCTOR), classController.delete)
 
 export default route
