38e50001ca9c3f6b4364ffebdc03cee94520fe09
Jhenie <highhalma@gmail.com>
2026-07-16 13:35:59 -0300

    FEAT: subject room middleware implemented

---
+++ api/src/shared/middlewares/subjectroom.middleware.ts
@@ -0,0 +1,66 @@
+import { NextFunction, Request, Response } from "express";
+import { prisma } from "../../lib/prisma.ts";
+import { ValidationError } from "../errors/ValidationError.ts";
+
+export const validateCreate = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { subjectId, roomId, priority } = req.body;
+
+        const relation = await prisma.subjectroom.findFirst({
+            where: { subjectId: subjectId, roomId: roomId },
+        });
+
+        if (!relation) {
+            throw new Error("Room already connected to the subject.");
+        }
+
+        if (priority !== undefined && priority < 1) {
+            throw new ValidationError("The priority must be greater than zero.");
+        }
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const validateDelete = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { subjectId, instructorId } = req.body;
+
+        const relation = await prisma.subjectinstructor.findFirst({
+            where: { subjectId: subjectId, instructorId: instructorId },
+        });
+
+        if (!relation) {
+            throw new Error("Instructor is not connected to the subject.");
+        }
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const validateUpdate = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const subjectRoomId: number = parseInt(req.params.id.toString());
+        const { subjectId, roomId, priority } = req.body;
+
+        const relation = await prisma.subjectroom.findFirst({
+            where: { id: subjectRoomId, subjectId: subjectId, roomId: roomId },
+        });
+
+        if (!relation) {
+            throw new Error("Room is not connected to the subject.");
+        }
+
+        if (priority !== undefined && priority < 1) {
+            throw new ValidationError("The priority must be greater than zero.");
+        }
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
