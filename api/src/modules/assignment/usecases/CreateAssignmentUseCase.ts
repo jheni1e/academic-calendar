aff3f3e7834e11c6aaebcb12ddf1bd204afe7ed0
Jhenie <highhalma@gmail.com>
2026-07-16 08:21:56 -0300

    FIX: middlewares migrated to use case

--- api/src/modules/assignment/usecases/CreateAssignmentUseCase.ts
+++ api/src/modules/assignment/usecases/CreateAssignmentUseCase.ts
@@ -1,6 +1,7 @@
 import { IAssignmentRepository } from "../repositories/IAssignmentRepository.ts";
 
 import { CreateAssignmentDTO } from "../AssignmentDTO.ts";
+import { ConflictError } from "../../../shared/errors/ConflictError.ts";
 
 export class CreateAssignmentUseCase {
     constructor(
@@ -8,6 +9,17 @@ export class CreateAssignmentUseCase {
     ) {}
 
     async execute(data: CreateAssignmentDTO) {
+        const { userId, roleId } = data;
+
+        const assignment = await this.assignmentRepository.findByUserAndRole(
+            userId,
+            roleId
+        );
+
+        if (assignment) {
+            throw new ConflictError("User already has this role.");
+        }
+
         return await this.assignmentRepository.create(data);
     }
 }
