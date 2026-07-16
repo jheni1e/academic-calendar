e9863d2fe27475e1a91eb8c07bc7d5d89a8efbd8
Jhenie <highhalma@gmail.com>
2026-07-16 10:26:43 -0300

    FEAT: event role middleware implemented

---
+++ api/src/shared/middlewares/eventrole.middleware.ts
@@ -0,0 +1,78 @@
+import { NextFunction, Request, Response } from "express";
+import { prisma } from "../../lib/prisma.ts";
+import { ConflictError } from "../errors/ConflictError.ts";
+import { NotFoundError } from "../errors/NotFoundError.ts";
+import { ForbiddenError } from "../errors/ForbiddenError.ts";
+
+export const validateCreate = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { name } = req.body;
+
+        if (!name.trim()) {
+            throw new Error("Class name is required.");
+        }
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const validateDelete = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const eventRoleId: number = parseInt(req.params.id.toString());
+
+        const eventRole =await prisma.eventrole.findFirst({
+            where: { id: eventRoleId },
+        });
+
+        if (!eventRole) {
+            throw new NotFoundError("Event role not found.");
+        }
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const validateUpdate = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { name } = req.body;
+        const eventRoleId: number = parseInt(req.params.id.toString());
+
+        const eventRole =await prisma.eventrole.findFirst({
+            where: { id: eventRoleId },
+        });
+
+        if (!eventRole) {
+            throw new NotFoundError("Event role not found.");
+        }
+
+        if (name !== undefined && !name.trim()) {
+            throw new Error("Event role name is mandatory.");
+        }
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const validateEventRoleExistsById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const eventRoleId: number = parseInt(req.params.id[0].toString());
+
+        const eventRole = await prisma.eventrole.findFirst({
+            where: { id: eventRoleId },
+        });
+
+        if (!eventRole) {
+            throw new NotFoundError("Event Role not found.");
+        }
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
