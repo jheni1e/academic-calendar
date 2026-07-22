5ec526fed026ee3f82eaea7ff12ef55c6b3521f6
FehFialho <fernandaklechowicz@gmail.com>
2026-07-22 08:53:57 -0300

    refact: remove unnecessary fields from reservation dto

--- api/src/dtos/reservationDto.ts
+++ api/src/dtos/reservationDto.ts
@@ -3,20 +3,15 @@ import { ReservationStatus } from "../generated/prisma/client.ts";
 export interface CreateReservationDTO {
     roomId: number;
     eventId: number;
-    scheduleStart: Date;
-    scheduleEnd: Date;
-    isBlocked?: boolean;
+
+    startDate: Date;
+    endDate: Date;
+
     description?: string;
 }
 
 export interface UpdateReservationDTO {
-    reservationId?: number
-    roomId?: number
-    eventId?: number
-    scheduleStart?: Date
-    scheduleEnd?: Date
-    status?: ReservationStatus;
-    isBlocked?: boolean
-    isConfirmed?: boolean
-    description?: string
+    reservationId: number;
+    roomId?: number;
+    description?: string;
 }
