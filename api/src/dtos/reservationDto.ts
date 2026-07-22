eb7606497c06f69bd65a8111a4f8a54e6505d32a
FehFialho <fernandaklechowicz@gmail.com>
2026-07-22 09:32:58 -0300

    refact: update fields on reservation dto

--- api/src/dtos/reservationDto.ts
+++ api/src/dtos/reservationDto.ts
@@ -11,7 +11,25 @@ export interface CreateReservationDTO {
 }
 
 export interface UpdateReservationDTO {
-    reservationId: number;
     roomId?: number;
+    eventId?: number;
+
+    startDate?: Date;
+    endDate?: Date;
+
+    status?: ReservationStatus;
+
+    isBlocked?: boolean;
+    isConfirmed?: boolean;
+
+    description?: string;
+}
+
+export interface UpdateReservationByEventDTO {
+    roomId: number;
+
+    startDate: Date;
+    endDate: Date;
+
     description?: string;
 }
