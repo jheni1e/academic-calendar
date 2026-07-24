fffb216821663657548f3989cbdcc4982bac7872
FehFialho <fernandaklechowicz@gmail.com>
2026-07-24 11:22:36 -0300

    feat: add scheduler middleware to route

--- api/src/routes/schedule.routes.ts
+++ api/src/routes/schedule.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { SchedulerController } from "../controllers/SchedulerController.ts";
+import { validateCreateLessonSeries } from "../shared/middlewares/scheduler.middleware.ts";
 
 const router = Router();
 
 router
-    .post("/lessons", SchedulerController.createLessonSeries);
+    .post("/lessons", validateCreateLessonSeries, SchedulerController.createLessonSeries);
 
 export default router;
