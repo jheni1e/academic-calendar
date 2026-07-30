19bb9b53b102c1dae84e585434eb6b5c8550d676
Leehzitha <leticia.burlinski@gmail.com>
2026-07-30 14:46:29 -0300

    feat: request errors padronized on user login

--- api/src/routes/auth.routes.ts
+++ api/src/routes/auth.routes.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import { AuthController } from '../controllers/AuthController.ts';
+import { validateLogin } from '../shared/middlewares/auth.middleware.ts';
 
 
 const route = express.Router();
 
 route 
-    .post('/login', AuthController.login)
+    .post('/login', validateLogin, AuthController.login)
 
 export default route
--- api/src/shared/middlewares/auth.middleware.ts
+++ api/src/shared/middlewares/auth.middleware.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { verifyToken } from "../../app/utils/jwt.ts";
+import { BadRequestError } from "../errors/BadRequestError.ts";
 
 export function authMiddleware(req: Request, res: Response, next: NextFunction){
     const authHeader = req.headers.authorization;
@@ -18,4 +19,20 @@ export function authMiddleware(req: Request, res: Response, next: NextFunction){
     } catch {
         return res.status(401).send({ message : "Invalid Token"})
     }
+}
+
+export function validateLogin(req: Request, res: Response, next: NextFunction) {
+    const { edv, password } = req.body
+
+    if(!req.body || Object.keys(req.body).length === 0)
+        throw new BadRequestError("Request body is required")
+
+    if(!edv) 
+        throw new BadRequestError("EDV is required")
+
+    if(!password)
+        throw new BadRequestError("Password is required")
+
+    next()
+
 }
