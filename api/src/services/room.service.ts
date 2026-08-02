7dc687ac61ba8db355cee19eb7b45e174f75b99e
Jhenie <highhalma@gmail.com>
2026-08-02 10:40:23 -0300

    FIX: fixed the get events by room response

--- api/src/services/room.service.ts
+++ api/src/services/room.service.ts
@@ -4,91 +4,161 @@ import { Room } from "../generated/prisma/client.ts";
 import { prisma } from "../lib/prisma.ts";
 import { NotFoundError } from "../shared/errors/NotFoundError.ts";
 
- 
 export const createRoom = async (data: CreateRoomDTO): Promise<Room> => {
-    return prisma.room.create({
-        data: {
-            title: data.title,
-            capacity: data.capacity,
-            description: data.description
-        }
-    });
-}
+  return prisma.room.create({
+    data: {
+      title: data.title,
+      capacity: data.capacity,
+      description: data.description,
+    },
+  });
+};
 
 export const findRoomById = async (roomId: number): Promise<Room | null> => {
-    return prisma.room.findUnique({
-        where: {
-            room_id: roomId
-        }
-    });
-}
+  return prisma.room.findUnique({
+    where: {
+      room_id: roomId,
+    },
+  });
+};
 
 export const findAllRooms = async (): Promise<Room[]> => {
-    return prisma.room.findMany();
-}
+  return prisma.room.findMany();
+};
 
 export const updateRoom = async (
-    roomId: number,
-    data: UpdateRoomDTO
+  roomId: number,
+  data: UpdateRoomDTO,
 ): Promise<Room> => {
-
-    return prisma.room.update({
-        where: {
-            room_id: roomId
-        },
-        data: {
-            description: data.description
-        }
-    });
-}
+  return prisma.room.update({
+    where: {
+      room_id: roomId,
+    },
+    data: {
+      description: data.description,
+    },
+  });
+};
 
 export const deleteRoom = async (roomId: number): Promise<void> => {
-    await prisma.room.delete({
-        where: {
-            room_id: roomId
-        }
-    });
-}
+  await prisma.room.delete({
+    where: {
+      room_id: roomId,
+    },
+  });
+};
 
-export const disableRoom = async (roomId : number) : Promise<void> => {
-    await prisma.room.update({
-        where: {
-            room_id: roomId
-        },
+export const disableRoom = async (roomId: number): Promise<void> => {
+  await prisma.room.update({
+    where: {
+      room_id: roomId,
+    },
 
-        data: {
-            is_active : false
-        }
-    })
-}
+    data: {
+      is_active: false,
+    },
+  });
+};
 
 export const getEventsByRoom = async (
-    roomId: number
+  roomId: number,
 ): Promise<EventResponseDTO[]> => {
+  return prisma.event.findMany({
+    where: {
+      reservation: {
+        room_id: roomId,
+      },
+    },
 
-    const room = await prisma.room.findUnique({
-        where: {
-            room_id: roomId
+    orderBy: {
+      start_date: "asc",
+    },
+
+    select: {
+      event_id: true,
+      title: true,
+      description: true,
+      start_date: true,
+      end_date: true,
+      event_type: true,
+      status: true,
+      is_blocked: true,
+
+      class: {
+        select: {
+          class_id: true,
+          name: true,
         },
-        include: {
-            reservations: {
-                include: {
-                    event: true
-                }
-            }
-        }
-    });
+      },
 
-    if (!room) {
-        return [];
-    }
+      recurrence: {
+        select: {
+          recurrence_id: true,
+          series_name: true,
+          repeat_until: true,
+          occurrences: true,
+          monday: true,
+          tuesday: true,
+          wednesday: true,
+          thursday: true,
+          friday: true,
+        },
+      },
 
-    return room.reservations.map(reservation => ({
-        id: reservation.event.event_id,
-        title: reservation.event.title,
-        description: reservation.event.description,
-        startDate: reservation.event.start_date,
-        endDate: reservation.event.end_date,
-        status: reservation.event.status
-    }));
-}
+      reservation: {
+        select: {
+          room: {
+            select: {
+              room_id: true,
+              title: true,
+              capacity: true,
+            },
+          },
+        },
+      },
+
+      subject_instructor: {
+        select: {
+          subject: {
+            select: {
+              subject_id: true,
+              name: true,
+            },
+          },
+
+          instructor: {
+            select: {
+              user_id: true,
+              user_edv: true,
+              name: true,
+              role: true,
+            },
+          },
+        },
+      },
+
+      participations: {
+        select: {
+          participation_id: true,
+          status: true,
+
+          user: {
+            select: {
+              user_id: true,
+              name: true,
+              user_edv: true,
+            },
+          },
+        },
+      },
+
+      creator: {
+        select: {
+          user_id: true,
+          name: true,
+          user_edv: true,
+        },
+      },
+    },
+  });
+};
