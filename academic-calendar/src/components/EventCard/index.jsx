d428754b9ec435ea21fca1462310d996221293cb
Jhenie <highhalma@gmail.com>
2026-08-02 10:29:21 -0300

    FIX: now each subject has its own color, making it easy to identify the lessons by subject

--- academic-calendar/src/components/EventCard/index.jsx
+++ academic-calendar/src/components/EventCard/index.jsx
@@ -1,65 +1,73 @@
-import { useState } from 'react';
-import './index.css';
-import Dialog from '../Dialog';
+import { useState } from "react";
+import "./index.css";
+import Dialog from "../Dialog";
 
-function getEventColor(id) {
+function getEventColor(event) {
   const colors = [
     "#007BC0",
     "#004975",
     "#00884A",
     "#9E2896",
     "#18837E",
-    "#BE0004"
+    "#BE0004",
   ];
+
+  const id =
+    event.subject_instructor?.subject?.subject_id ??
+    event.event_id;
 
   return colors[id % colors.length];
 }
-
 
 function EventCard({ event, compact, refreshEvents }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [dialogType, setDialogType] = useState("view-event");
 
   const changeModal = async () => {
-    setIsModalOpen(!isModalOpen)
-    setDialogType("view-event")
-    
+    setIsModalOpen(!isModalOpen);
+    setDialogType("view-event");
+
     if (refreshEvents) {
       await refreshEvents();
     }
-    
-  }
+  };
 
-
-  const color = getEventColor(event.event_id);
+  const color = getEventColor(event);
 
   const start = new Date(event.start_date);
 
   const time = start.toLocaleTimeString("pt-BR", {
     hour: "2-digit",
-    minute: "2-digit"
+    minute: "2-digit",
   });
 
   return (
     <>
       <div
         className={`event-card ${compact ? "compact" : ""}`}
-        style={{ "--event-color": color, opacity: event.status === "SCHEDULED" ? 0.4 : 1 }}
+        style={{
+          "--event-color": color,
+          opacity: event.status === "SCHEDULED" ? 0.4 : 1,
+        }}
         onClick={() => changeModal()}
-        >
+      >
         {!compact && (
           <>
-            <span className="event-title">
-              {event.title}
-            </span>
+            <span className="event-title">{event.title}</span>
           </>
         )}
       </div>
-      {isModalOpen &&
-        <Dialog event={event} isOpen={isModalOpen} onClose={changeModal} title={event.title} type={dialogType} setType={setDialogType}></Dialog>
-      }
+      {isModalOpen && (
+        <Dialog
+          event={event}
+          isOpen={isModalOpen}
+          onClose={changeModal}
+          title={event.title}
+          type={dialogType}
+          setType={setDialogType}
+        ></Dialog>
+      )}
     </>
-
   );
 }
 
